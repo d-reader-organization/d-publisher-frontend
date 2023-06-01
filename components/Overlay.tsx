ad97a20a8ae19a4a03f0b93d58f8b2d71caac71b
nborovic <ninoborovic.nb@gmail.com>
2023-06-01 17:10:13 +0200

    feature: clean up the project and start implementing layout

--- components/Overlay.tsx
+++
@@ -1,8 +0,0 @@
-import { Box, BoxProps } from '@mui/material'
-import clsx from 'clsx'
-
-const Overlay: React.FC<BoxProps> = ({ className, ...props }) => {
-	return <Box className={clsx('overlay', className)} {...props} />
-}
-
-export default Overlay
