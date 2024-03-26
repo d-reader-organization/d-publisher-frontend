ce6fb086b133b380d7fd3e6561636f98cbb8e1d5
d-reader-luka <luka.crnogorac@dreader.io>
2024-03-26 17:00:28 +0100

    chore: enable comic title editing

--- api/comic/queries/useUpdateComic.ts
+++ api/comic/queries/useUpdateComic.ts
@@ -3,6 +3,7 @@ import { useToaster } from 'providers/ToastProvider'
 import { BasicComic, UpdateComicData } from 'models/comic'
 import { useMutation, useQueryClient } from 'react-query'
 import http from 'api/http'
+import { useRouter } from 'next/navigation'
 
 const { COMIC, UPDATE } = COMIC_QUERY_KEYS
 
@@ -14,11 +15,15 @@ const updateComic = async (slug: string, request: UpdateComicData): Promise<Basi
 export const useUpdateComic = (slug: string) => {
 	const toaster = useToaster()
 	const queryClient = useQueryClient()
+	const router = useRouter()
 
 	return useMutation({
 		mutationFn: (request: UpdateComicData) => updateComic(slug, request),
-		onSuccess: () => {
+		onSuccess: ({ slug: newSlug }) => {
 			toaster.add('Comic updated!', 'success')
+			if (slug !== newSlug) {
+				return router.replace(`/comic/${newSlug}/edit`)
+			}
 			queryClient.invalidateQueries([COMIC_QUERY_KEYS.COMIC, COMIC_QUERY_KEYS.GET_RAW])
 		},
 		onError: toaster.onQueryError,
